97896ee4c74f728fce2d45f7234c2ac04a11dcff
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2021-12-21 16:34:35 +0200

    task 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+{/* <a class = gallery__link href="" data-lightbox="lbox"> */}
 console.log(galleryItems);
 
 function createImageGallery(){
     return galleryItems.map(({preview,original,description})=>{
-        return `
-        <a class = gallery__link>
-        <img class=gallery__image
-        src=${preview} 
-        data-big=${original} 
-        alt=${description}>
-        </a>
+        return `     
+<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+</div>
         `
     }).join('')
     }
@@ -19,15 +23,33 @@ function createImageGallery(){
     
     gallery.insertAdjacentHTML('beforeend',createImageGallery())
     
-    gallery.addEventListener('click', onImageClick)
-    
-    function onImageClick (e){
-        const galleryImage = e.target.classList.contains('gallery__image');
-    if(!galleryImage){
-        return;
+
+gallery.addEventListener('click', onImageClick)
+function onImageClick(e) {
+    e.preventDefault();
+    const galleryImage = e.target.classList.contains('gallery__image');
+        if(!galleryImage){
+            return;
+        }  
+
+const bigImg = e.target.dataset.source
+    const modalImg = basicLightbox.create(
+        `<img width="1400" height="900" src="${bigImg}">`
+    );
+    modalImg.show();
+
+
+    window.addEventListener('keydown', closeModal);
+
+    function closeModal(e) {
+        if (e.code === 'Escape') {
+            modalImg.close();
+             window.removeEventListener('keydown', closeModal)
+        }
     }
-    const bigImage= e.target.dataset.big
-    
-    console.log(bigImage)
-    
-    }
+}
+
+
+
+
+ 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+function createImageGallery(){
+    return galleryItems.map(({preview,original,description})=>{
+        return `     
+<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+</div>
+        `
+    }).join('')
+}
+const gallery = document.querySelector('.gallery')
+    
+gallery.insertAdjacentHTML('beforeend',createImageGallery())
+
+let lightbox = new SimpleLightbox('.gallery a',{captionDelay:'250ms', captionsData:'alt'});
+lightbox.close()
